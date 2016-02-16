13cf34017982340fd0ea9d83bc2f6a7855c13ae6
tupac56 <juan.tupac56@gmail.com>
2016-02-16 15:20:15 +0100

    Create groupfunctions.h

---
+++ prog/groupfunctions.h
@@ -0,0 +1,14 @@
+#ifndef GROUPFUNCTIONS_H
+#define GROUPFUNCTIONS_H
+
+#define PI 3.14159265
+
+// Tupac: 2D mesh rotation
+void rotation2D(Mesh *mesh, float angle);
+
+// Tupac: defines the center of a 2D mesh
+// I don't know if it works
+void center2D(Mesh *mesh, float *xc, float *yc);
+
+
+#endif
