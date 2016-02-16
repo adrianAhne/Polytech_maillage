b90c89ceaff5091b255115e3e834890b4427d821
tupac56 <tupacquiroga@MacBook-Pro-de-Tupac.local>
2016-02-16 16:25:04 +0100

    Test 3D rotation

--- prog/groupfunctions.c
+++ prog/groupfunctions.c
@@ -13,9 +13,8 @@ void rotation2D(Mesh *mesh, float angle)
 {
 	pPoint ppt;
 	int i;
-	float xc, yc;
 
-	printf("Centres : %f %f\n", xc, yc);
+	//printf("Centres : %f %f\n", xc, yc);
 	for (i = 0; i <= mesh->np; i++)
 	{
 		ppt = &mesh->point[i];
@@ -28,6 +27,46 @@ void rotation2D(Mesh *mesh, float angle)
 
 		ppt->c[0] = x;
 		ppt->c[1] = y;
+	}
+}
+
+void rotation3D(Mesh *mesh, float angleX, float angleY, float angleZ)
+{
+	pPoint ppt;
+	int i;
+
+	float x,y,z;
+	
+	for (i = 0; i <= mesh->np; i++)
+	{
+		ppt = &mesh->point[i];
+		
+		
+		// Si l'angle est sufisamment grand pour que la rotation puisse se faire
+		if (fabsf(angleX) > 1e-10)
+		{
+			y = ppt->c[1] * cos(angleX) - ppt->c[2] * sin(angleX);
+			z = ppt->c[1] * sin(angleX) + ppt->c[2] * cos(angleX);
+			ppt->c[1] = y;
+			ppt->c[2] = z;
+		}
+
+		if (fabsf(angleY) > 1e-10)
+		{
+			x = ppt->c[2] * sin(angleY) + ppt->c[0] * cos(angleY);
+			z = ppt->c[2] * cos(angleY) - ppt->c[0] * sin(angleY);
+			ppt->c[0] = x;
+			ppt->c[2] = z;
+		}
+
+		if (fabsf(angleZ) > 1e-10)
+		{
+			x = ppt->c[0] * cos(angleZ) - ppt->c[1] * sin(angleZ);
+			y = ppt->c[0] * sin(angleZ) + ppt->c[1] * cos(angleZ);
+			ppt->c[0] = x;
+			ppt->c[1] = y;
+		}
+		
 	}
 }
 
